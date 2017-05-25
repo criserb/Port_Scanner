16b2de2267d0be1cf099cc6c60ad9ca580012632
student <student@PC-224>
2017-05-25 16:20:57 +0200

    added functionality

--- Port_Scanner/Form1.cs
+++ Port_Scanner/Form1.cs
@@ -21,11 +21,8 @@ namespace Port_Scanner
 
         private void btn_Start_Click(object sender, EventArgs e)
         {
-            short[] ListaPortow = new short[30000];
-            for (int i = 20; i < 29800; i++)
-            {
-                ListaPortow[i - 20] = i;
-            }
+            short[] ListaPortow = { 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30 };
+
             string host = textBox1.Text;
             listBox1.Items.Add($"Skanowanie portów dla {host}");
             listBox1.Items.Add("To może chwilę potrwać...");
@@ -35,7 +32,11 @@ namespace Port_Scanner
                 try
                 {
                     TcpClient client = new TcpClient(host, port);
-                    listBox1.Items.Add()
+                    listBox1.Items.Add($"Port: {port.ToString()} jest otwarty");
+                }
+                catch
+                {
+                    listBox1.Items.Add($"Port {port.ToString()} jest zamknięty!");
                 }
             }
         }
